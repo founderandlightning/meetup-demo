da43f1e36c4bb87af4f20fab1f1e371d2c3c16b3
Jaspinder Pawar <jaspinder@ucreate.co.in>
2019-02-15 12:21:59 +0530

    Modified text

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('index', {
-        title: 'How to do code review',
+        title: 'Code reviewee',
     });
 });
 
