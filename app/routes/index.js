b542c83ffc2bfa5101363c4a82a3485ea430fc6e
Jaspinder Pawar <jaspinder@ucreate.co.in>
2019-02-15 17:00:12 +0530

    title changed

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,11 @@
-const express = require('express')
+const express = require("express");
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index', {
-        title: 'Code reviewee',
-    });
+router.get("/", (req, res) => {
+  res.render("index", {
+    title: "Code reviewee tool"
+  });
 });
 
 module.exports = router;
