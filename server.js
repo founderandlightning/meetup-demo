ffc629140f56cede5b397b740ab6f499fbacff8b
JaspinderPawar <jaspinder@ucreate.co.in>
2019-07-01 12:36:36 +0530

    Revert "added two"
    This reverts commit 8d4d8dca12db922589983d8498343b2c03ef4944.

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get("/", index);
 
 app.get("/calculate", (req, res) => {
   try {
-    throw new Error("Can't divide by zero to one two");
+    throw new Error("Can't divide by zero to one");
   } catch (error) {
     rollbar.error(error);
   }
