8d4d8dca12db922589983d8498343b2c03ef4944
JaspinderPawar <jaspinder@ucreate.co.in>
2019-07-01 12:35:48 +0530

    added two

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get("/", index);
 
 app.get("/calculate", (req, res) => {
   try {
-    throw new Error("Can't divide by zero to one");
+    throw new Error("Can't divide by zero to one two");
   } catch (error) {
     rollbar.error(error);
   }
